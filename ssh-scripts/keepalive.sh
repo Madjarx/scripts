bce3edf30f6a295ce5a59f72c2b3930f08c13759
Madjarx <lukaradisavljevic30@gmail.com>
2022-12-27 19:50:45 +0100

    Update on script generated logs

--- ssh-scripts/keepalive.sh
+++ ssh-scripts/keepalive.sh
@@ -29,14 +29,18 @@ if [ -f $HOME/.ssh/config ]; then
   echo "Config file found"
 else
   # If the config file does not exist, echo that it is not found
-  echo "Config file not found"
+  echo "Config file not found. Creating one for you!"
   # Create the config file
   touch ~/.ssh/config
   # Change the permissions of the config file to 600
   chmod 600 ~/.ssh/config
+  
+  echo "Config file created"
 fi
 
 # Edit the config file to send an opt null package from the client side to the remote ssh server every 60 seconds
+echo "Setting the interval to $interval"
 echo "Host *" >> ~/.ssh/config
 echo "  KeepAlive yes" >> ~/.ssh/config
 echo "  ServerAliveInterval $interval" >> ~/.ssh/config
+echo "Done!"
